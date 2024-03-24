cf26c4205b534c0fb91358b59af24edd20e7f68a
Devohka <110058157+Devohka@users.noreply.github.com>
2024-03-24 14:29:56 +0200

    qwert

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const noBtn = document.querySelector(".haha-no");
 const yesBtn = document.querySelector(".haha-yes");
-
+const pageEl = document.querySelector(".haha-page");
 
 
 yesBtn.addEventListener("click", () => {
@@ -9,4 +9,5 @@ yesBtn.addEventListener("click", () => {
 
 noBtn.addEventListener("click", () => {
     noBtn.style.display = "none";
+    pageEl.style.display = "block";
 });
