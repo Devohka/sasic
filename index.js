cca9b61ee3fae89874884f2ac67717e05043484d
Devohka <110058157+Devohka@users.noreply.github.com>
2024-03-24 13:43:56 +0200

    haha

---
+++ index.js
@@ -0,0 +1,12 @@
+const noBtn = document.querySelector(".haha-no");
+const yesBtn = document.querySelector(".haha-yes");
+
+
+
+yesBtn.addEventListener("click", () => {
+    alert("я знаю))))");
+});
+
+noBtn.addEventListener("click", () => {
+    noBtn.style.display = "none";
+});
